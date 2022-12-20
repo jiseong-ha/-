15f64898b60762c95db857eb34486e38a8262f9e
jiseong-ha <112460092+jiseong-ha@users.noreply.github.com>
2022-12-20 22:02:54 +0900

    push 사용해보기

--- sketch.js
+++ sketch.js
@@ -1,37 +1,18 @@
-
-let x, y;
-x = 0;
-y = 0;
-
-let w = 20;
-let p = 0.5;
-
 function setup() {
   createCanvas(400, 400);
-  background(0);
-  stroke(200);
+  background(255);
+  strokeWeight(2);
+  stroke(0, 50);
 }
 
 function draw() {
+  if (mouseIsPressed ) {
   
-  if (random() > p) {
-    stroke(random(255), random(255), random(255));
-  strokeWeight(1);
-  line(width/2, height/2, mouseX, mouseY);
-  } else {
-  	line(mouseX, mouseY, x, y+w);
-  }
-  
-  x = x + w;
-  
-  if (x > width) {
-    y = y + w;
-    x = 0;
-  }
-  
-  if (y > height) {
-    background(0);
-    x = 0;
-    y = 0;
+ push(); 
+strokeWeight(5);
+fill(0);
+ellipse(width/2, height/2,55,55 ); 
+
+    pop();
   }
 }
