260651057590ecb361ebbb80d5659f73a0a82cef
jiseong-ha <112460092+jiseong-ha@users.noreply.github.com>
2022-12-20 19:52:03 +0900

    새로운 OOP 도입 실패 (1)

--- 새 폴더/sketch.js
+++ 새 폴더/sketch.js
@@ -1,4 +1,6 @@
 let P;
+let angle1 = random(0, 6 * PI);  
+let angle2 = random(0, 6 * PI); 
 
 function setup() {
  createCanvas(400, 400);
@@ -9,22 +11,33 @@ function setup() {
 }
 
 function draw() {
- 
   P.update ();
+  P.display ();
 }
 
-function Play() {
-  stroke(0, 0, 0, 15);
-  this.update = function()
-  {let angle1 = random(0, 6 * PI);
-  let xpos1 = 200 + 200 * cos(angle1);
-  let ypos1 = 200 + 200 * sin(angle1);
- 
-  let angle2 = random(0, 6 * PI);
-  let xpos2 = 200 + 200 * cos(angle2);
-  let ypos2 = 200 + 200 * sin(angle2);
+class Play {
+  
+  constructor(){  
+    
+    this.xpos1 = 200 + 200 * cos(angle1);
+   this.ypos1 = 200 + 200 * sin(angle1); 
 
-  line(xpos1, ypos1, xpos2, ypos2);}
+  this. xpos2 = 200 + 200 * cos(angle2);
+   this.ypos2 = 200 + 200 * sin(angle2); 
+ 
+  }
+ 
+ update(){  
+
+ 
+    }
+
+ 
+ 
+display(){ 
+  stroke(0, 0, 0, 15);
+line(xpos1, ypos1, xpos2, ypos2);
+}
 
   
 }
