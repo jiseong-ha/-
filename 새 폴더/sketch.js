d95043de56676e2a0f293b13f28239699e1e0817
jiseong-ha <112460092+jiseong-ha@users.noreply.github.com>
2022-12-20 19:32:06 +0900

    기존 예제를 객체 기반으로 바꾸기

--- 새 폴더/sketch.js
+++ 새 폴더/sketch.js
@@ -1,19 +1,22 @@
-
+let P;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 255, 255);
-  stroke(0, 0, 0, 15);
+ createCanvas(400, 400);
+ background(255, 255, 255);
+ P = new Play();
+ 
+  
 }
 
 function draw() {
-
-  randomChord();
-  randomChord();
+ 
+  P.update ();
 }
 
-function randomChord() {
-  let angle1 = random(0, 6 * PI);
+function Play() {
+  stroke(0, 0, 0, 15);
+  this.update = function()
+  {let angle1 = random(0, 6 * PI);
   let xpos1 = 200 + 200 * cos(angle1);
   let ypos1 = 200 + 200 * sin(angle1);
  
@@ -21,5 +24,7 @@ function randomChord() {
   let xpos2 = 200 + 200 * cos(angle2);
   let ypos2 = 200 + 200 * sin(angle2);
 
-  line(xpos1, ypos1, xpos2, ypos2);
+  line(xpos1, ypos1, xpos2, ypos2);}
+
+  
 }
