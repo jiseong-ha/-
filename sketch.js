2d4f609d949d32da490c66c0b0aab3077b6ff5f1
jiseong-ha <112460092+jiseong-ha@users.noreply.github.com>
2022-12-20 22:34:19 +0900

    예제 사용해보기

--- sketch.js
+++ sketch.js
@@ -1,18 +1,23 @@
+let p = 0.5;
+
 function setup() {
   createCanvas(400, 400);
-  background(255);
-  strokeWeight(2);
-  stroke(0, 50);
+  background(0);
+  stroke(200);
 }
 
 function draw() {
-  if (mouseIsPressed ) {
   
- push(); 
-strokeWeight(5);
-fill(0);
-ellipse(width/2, height/2,55,55 ); 
-
-    pop();
+  if (random() > p) {
+  	  point(random(500), random(400));
+  } else {
+  	stroke(random(255), random(255), random(255));
+  strokeWeight(1);
+  line(width/2, height/2, mouseX, mouseY);
   }
+  }
+  
+function keyReleased() {
+  if (keyCode == DELETE) background(255);
+ 
 }
