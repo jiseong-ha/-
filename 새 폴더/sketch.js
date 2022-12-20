7e51983cf1df52ea43953644476b437e283f7dac
jiseong-ha <112460092+jiseong-ha@users.noreply.github.com>
2022-12-20 20:46:18 +0900

    왜 검은 화면 밖에 안보일까

--- 새 폴더/sketch.js
+++ 새 폴더/sketch.js
@@ -3,38 +3,30 @@ let P;
 
 function setup() {
  createCanvas(400, 400);
-background(255, 255, 255);
+ background(255, 255, 255);
  P = new Play();
  
   
 }
 
 function draw() {
-  
-  if (mouseIsPressed === true) {fill(0);
-    rect (0,0,400,400);
-    
-  }
-  else {
-    P.update ();
-  
-  }
-
+  P.update ();
 
 }
 
 function Play() {
  
-  stroke(0, 0, 0, 15);
-
  this. update = function(){
+if (mouseIsPressed === true) {stroke(0, 0, 0, 15);
   let angle1 = random(0, 6 * PI); 
    let xpos1 = 200 + 200 * cos(angle1);
    let ypos1 = 200 + 200 * sin(angle1);
   let angle2 = random(0, 6 * PI);
     let xpos2 = 200 + 200 * cos(angle2);
    let ypos2 = 200 + 200 * sin(angle2); 
+line(xpos1, ypos1, xpos2, ypos2); }
   
-line(xpos1, ypos1, xpos2, ypos2); 
+  else{ background (0);}
+  
  }
 }
